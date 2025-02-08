e4f28ccee2cd8dafd55aed88893668a34f6335f3
Hunter Wilhelm <71348224+hunterwilhelm@users.noreply.github.com>
2025-02-07 18:05:50 -0800

    added nuxt example

---
+++ examples/nuxt/app.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+
+const { data: hello, suspense } = useTRPC().hello.useQuery({ text: 'client' })
+
+onServerPrefetch(async () => {
+  await suspense()
+})
+
+</script>
+
+<template>
+  <div>
+    <p>{{ hello?.greeting }}</p>
+  </div>
+</template>
